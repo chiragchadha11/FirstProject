70eb3186bd162a1801d5d66d1d59c2f8ba766a1d
chiragchadha11 <cpchadha@hotmail.com>
2023-06-28 21:50:31 -0400

    second commit

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,7 +1,7 @@
 public class ForLoop {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 11; i++){
 
             sum += i;
         }
